d6e5a026fd0a32388672944ca63ad263fb04f916
Nico Walser <nicrwal@gmail.com>
2020-12-26 13:10:44 -0800

    updating shop page

--- src/App.js
+++ src/App.js
@@ -2,11 +2,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import Homepage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-      <Homepage />
+      <Switch>
+        <Route exact path='/' component={Homepage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   );
 }
